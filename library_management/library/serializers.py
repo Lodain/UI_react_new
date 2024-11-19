2eca7ff02422f3ad1fde2214e93745d1ca74ee97
Danilo <danilospera@gmail.com>
2024-11-19 20:37:22 +0100

    backend

---
+++ library_management/library/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from .models import Book, Author, Genre, Review, LendedBook, Wishlist
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
+    authors = AuthorSerializer(many=True, read_only=True)
+    genres = GenreSerializer(many=True, read_only=True)
+    remaining_copies = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def get_remaining_copies(self, obj):
+        return obj.copies - obj.lended
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class LendedBookSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = LendedBook
+        fields = '__all__'
+
+class WishlistSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Wishlist
+        fields = '__all__'
