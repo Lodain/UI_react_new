8fc5bb920ff0e6dc9c63451adadc64d81b42199f
Danilo <danilospera@gmail.com>
2024-11-21 09:50:15 +0100

    Added material design component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import Borrow from './Borrow';
 import axiosInstance from './axiosConfig';
 import Account from './account';
 import EmailVerification from './EmailVerification';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import './App.css';
 
 function App() {
   const [user, setUser] = useState(null);
   const [details, setDetails] = useState([]);
 
   useEffect(() => {
-    // Check if user information is stored in session storage
     const storedUser = JSON.parse(sessionStorage.getItem('user'));
     if (storedUser) {
       setUser(storedUser);
@@ -33,11 +34,22 @@ function App() {
           <h1>Library Management System</h1>
           {user && <h2>Welcome back, {user.username}!</h2>}
           {details.map((output, id) => (
-            <div key={id}>
-              <img src={`http://127.0.0.1:8080${output.cover}`} alt={output.title} />
-              <h2>{output.title}</h2>
-              <p>Authors: {output.authors.join(', ')}</p>
-            </div>
+            <Card key={id} style={{ maxWidth: 345, margin: '20px auto' }}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={`http://127.0.0.1:8080${output.cover}`}
+                alt={output.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {output.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Authors: {output.authors.join(', ')}
+                </Typography>
+              </CardContent>
+            </Card>
           ))}
         </>
       )}
