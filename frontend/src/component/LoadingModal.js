1fb7e536bbaf583e37a544bab40d75f3bd981153
Danilo <danilospera@gmail.com>
2024-12-04 18:09:51 +0100

    LoadingModal

--- frontend/src/component/LoadingModal.js
+++ frontend/src/component/LoadingModal.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import '../style/LoadingModal.css';
+import React from 'react'; 
+import '../style/LoadingModal.css'; // Import the CSS 
 
+// Functional component for the loading modal
 const LoadingModal = ({ show }) => {
+  // If 'show' prop is false, don't render anything
   if (!show) return null;
 
+  // Render the modal with a spinner and loading text
   return (
     <div className="loading-modal">
       <div className="loading-content">
-        <div className="spinner"></div> 
-        <p>Loading...</p>
+        <div className="spinner"></div> {/* Spinner for visual loading indication */}
+        <p>Loading...</p> 
       </div>
     </div>
   );
